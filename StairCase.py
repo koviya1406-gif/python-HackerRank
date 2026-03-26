fb1b389ff7dd0154baa7f7dc8fb01d70ff2626b7
Oviya K <koviya1406@gmail.com>
2026-03-26 12:23:56 +0530

    Starecase

---
+++ StairCase.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+def staircase(n):
+    for i in range(1, n + 1):
+        spaces = ' ' * (n - i)
+        hashes = '#' * i
+        print(spaces + hashes)
+if __name__ == '__main__':
+    n = int(input().strip())
+    staircase(n)
