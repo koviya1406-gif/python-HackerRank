a8d1dbb9f57688b60f593da7817d7f4d1a722441
Oviya K <koviya1406@gmail.com>
2026-04-16 11:27:53 +0530

    IterTools

---
+++ iter.py
@@ -0,0 +1,7 @@
+
+from itertools import combinations_with_replacement
+io = input().split()
+s = sorted(io[0]) 
+k = int(io[1])
+for combo in combinations_with_replacement(s, k):
+    print("".join(combo))
