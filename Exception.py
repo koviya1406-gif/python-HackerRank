cb91e9a5a24905a68248f4971409225540942519
Oviya K <koviya1406@gmail.com>
2026-04-16 12:26:58 +0530

    Exception

---
+++ Exception.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n):
+    try:
+        a, b = map(int, input().split())
+        print(a // b)
+    except ZeroDivisionError:
+        print("Error Code: integer division or modulo by zero")
+    except ValueError as e:
+        print("Error Code:", e)
